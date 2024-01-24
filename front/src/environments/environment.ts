92c44682804fe82f938d3448f6e191f7e51ffcbc
Maxime <blaessmaxime@gmail.com>
2024-01-24 15:05:58 +0100

    Nouveau

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    backendConnection: 'http://bdd-max.onrender.com/api/utilisateur/login',
-    backendInscription: 'http://bdd-max.onrender.com/api/utilisateur/accountcreation',
+    backendConnection: 'https://bdd-max.onrender.com/api/utilisateur/login',
+    backendInscription: 'https://bdd-max.onrender.com/api/utilisateur/accountcreation',
     backendProduit: 'https://bdd-max.onrender.com/api/produit/getcatalog'
   };
   
