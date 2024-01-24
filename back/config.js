10043527d7256541427a7b2209a983466b347aad
Maxime <blaessmaxime@gmail.com>
2024-01-24 10:52:32 +0100

    finition

--- back/config.js
+++ back/config.js
@@ -1,7 +1,8 @@
 module.exports = {
+    ACCESS_TOKEN_SECRET: "EMMA123",
     BDD: {
         "host": "dpg-cm23ilen7f5s73erfi30-a.frankfurt-postgres.render.com",
-        "port": "5432",
+        "port": "",
         "user": "postgree_max_user",
         "password": "iuslL84D8iqcVPRC4IA26I4l3dXdBAg6",
         "bdname": "postgree_max"
